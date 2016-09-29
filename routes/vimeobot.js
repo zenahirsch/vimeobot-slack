3efde2f10b9c9875278685e195d0cc26a2a7d241
Hirsch <hirschz@VIMNY-HIRSCZ-LM.vimeo.iac.corp>
2016-09-29 11:58:20 -0400

    begin vimeo implementation

--- routes/vimeobot.js
+++ routes/vimeobot.js
@@ -1,16 +1,35 @@
 var express = require('express');
+var Vimeo = require('vimeo').Vimeo;
 var router = express.Router();
+
+var lib = new Vimeo(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.ACCESS_TOKEN);
 
 /* GET vimeobot index. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ 	res.render('index', { title: 'Express' });
 });
 
-router.post('/', function(req, res, next) {
-  res.status(200).json({
-  	"response_type": "in_channel",
-  	"text": "hello! https://vimeo.com/183752285"
-  });
+router.post('/', (req, res, next) => {
+	let video = null;
+
+	lib.request({
+		path: '/channels/staffpicks/videos',
+		query: {
+			per_page: 50
+		}
+	}, (error, body, status_code, headers) => {
+		if (error) {
+			res.status(500).json({
+				"text": `There was an error ${error}`
+			});
+		} else {
+			video = body.data[1]; // will be randomized
+			res.status(200).json({
+		 		"response_type": "in_channel",
+		  		"text": video.link
+		  	});
+		}
+	});
 });
 
 module.exports = router;
