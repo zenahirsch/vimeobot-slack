ac85c8d14f3a24cad99f281ad8762f667c90cb99
Hirsch <hirschz@VIMNY-HIRSCZ-LM.vimeo.iac.corp>
2016-09-29 15:17:14 -0400

    add path choices

--- routes/vimeobot.js
+++ routes/vimeobot.js
@@ -4,13 +4,14 @@ var router = express.Router();
 
 var lib = new Vimeo(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.ACCESS_TOKEN);
 
-var makeRequest = function (res, path, page, per_page, fields, callback) {
+var makeRequest = function (res, path, page, per_page, fields, query, callback) {
 	lib.request({
 		path: path,
 		query: {
 			page: page,
 			per_page: per_page,
-			fields: fields
+			fields: fields,
+			query: query
 		}
 	}, (error, body, status_code, headers) => {
 		if (error) {
@@ -29,22 +30,37 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', (req, res, next) => {
-	console.log(req.body);
-	res.status(200).json({
- 		'response_type': 'in_channel',
-  		'text': req.body
-  	});
-/*
-	makeRequest(res, '/channels/927', 1, null, 'metadata.connections.videos.total', (body) => {
+	var paths = {
+		'staffpick': '/channels/927',
+		'staffpicks': '/channels/927',
+		'comedy': '/categories/comedy',
+		'funny': '/categories/comedy',
+		'weird': '/categories/experimental',
+		'experimental': '/categories/experimental',
+		'animation': '/categories/animation',
+		'documentary': '/categories/documentary',
+		'food': '/categories/food'
+	};
+
+	var path = null;
+	var query = null;
+
+	if (paths[req.body.text]) {
+		path = paths[req.body.text];
+	} else {
+		path = '/videos';
+		query = req.body.text;
+	}
+
+	makeRequest(res, path, 1, null, 'metadata.connections.videos.total', query, (body) => {
 		var rand_page = Math.floor(Math.random() * body.metadata.connections.videos.total) + 1;
-		makeRequest(res, '/channels/927/videos', rand_page, 1, 'link', (body) => {
+		makeRequest(res, `${path}/videos`, rand_page, 1, 'link', query, (body) => {
 			res.status(200).json({
 		 		'response_type': 'in_channel',
-		  		'text': body.data[0].link
+		  		'text': `Hey ${req.body.user_name}, here's your video! ${body.data[0].link}`
 		  	});
 		});
 	});
-*/
 });
 
 module.exports = router;
